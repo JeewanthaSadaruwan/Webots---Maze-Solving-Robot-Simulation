1aa92834b8d326e9c2fba08d2fd3607768a3d105
Jeewantha Sadaruwan <jeewansadaruwan987@gmail.com>
2025-01-19 23:22:55 +0530

    Delete Robogames directory

--- Robogames/controllers/finalocde/Robot/__init__.py
+++
@@ -1,7 +0,0 @@
-from Robot.sensors import Sensor
-from Robot.calibration import calibration
-from Robot.maze_navigation import navigation
-from Robot.control import Control
-
-
-_all_ = ['Sensor' , 'navigation', 'Control','calibration']
--- Robogames/controllers/finalocde/Robot/floodfill.py
+++
@@ -1,7 +0,0 @@
-# Initialize a 10x10 grid with coordinates
-rows, cols = 10, 10
-maze_coordinates = [[(i, j) for j in range(cols)] for i in range(rows)]
-
-# Print the grid with coordinates
-for row in maze_coordinates:
-    print(row)
